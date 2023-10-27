48657198f03434ac5bf2b2efc2d35d3f779294b2
realth000 <a598919335@gmail.com>
2023-10-27 13:33:45 +0800

    Update

--- src/Chapter9/IO.hs
+++ src/Chapter9/IO.hs
@@ -1,11 +1,17 @@
 module Chapter9.IO (runChapter9) where
 
+import Control.Monad
+import GHC.IO.IOMode (IOMode(ReadMode))
+import Control.Exception (handle)
+import System.IO
+
 runIOActionBinding = do
   putStrLn "Input a number:"
-  name <- getLine
+  name <- getLine 
   putStrLn ("Number is " ++ name)
 
 runIOEcho = do
+  putStrLn "Input number"
   line <- getLine
   if null line
     then return ()
@@ -13,5 +19,28 @@ runIOEcho = do
       putStrLn line
       runIOEcho
 
+runGetChar = do
+  c <- getChar
+  when (c /= 'q') $ do
+    putChar c
+    runGetChar
+
+runGet3Char = do
+  putStrLn "Input 3 chars"
+  c3 <- sequence [getLine, getLine, getLine]
+  print c3
+
+runForever = forever $ do
+  putStr "Give me some input: "
+  l <- getLine
+  putStrLn l
+
+runReadFile = do
+  putStrLn "Input file name:"
+  name <- getLine
+  withFile name ReadMode (\handle -> do
+    contents <- hGetContents handle
+    putStr contents)
+
 runChapter9 :: IO ()
-runChapter9 = runIOEcho
+runChapter9 = runReadFile
